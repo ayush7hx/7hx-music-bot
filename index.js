127b0c1de3798df9f9e608c4f1198fbaf2ec54ae
Ayush Arya <ayusharyaft63@gmail.com>
2026-02-14 13:34:05 +0530

    Create index.js file with main bot initialization code

---
+++ index.js
@@ -0,0 +1,26 @@
+// index.js
+
+const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+// Load commands
+client.commands = new Map();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+// Load events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    client.on(event.name, (...args) => event.execute(...args, client));
+}
+
+// Log in to Discord
+client.login('YOUR_BOT_TOKEN');
